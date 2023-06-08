948dcacbdb27b1638f475d3976d39e8a7adc02be
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-06-08 11:30:13 +0200

    Finish example

--- Search.pm
+++ Search.pm
@@ -227,6 +227,8 @@ Returns Plack::Component object.
 
 =head1 EXAMPLE
 
+=for comment filename=search_app.pl
+
  use strict;
  use warnings;
 
@@ -238,6 +240,7 @@ Returns Plack::Component object.
  # Run application.
  my $app = Plack::App::Search->new(
          'css' => CSS::Struct::Output::Indent->new,
+         'generator' => 'Plack::App::Search',
          'tags' => Tags::Output::Indent->new(
                  'preserved' => ['style'],
                  'xml' => 1,
@@ -249,7 +252,62 @@ Returns Plack::Component object.
  # HTTP::Server::PSGI: Accepting connections at http://0:5000/
 
  # > curl http://localhost:5000/
- # TODO
+ # <!DOCTYPE html>
+ # <html lang="en">
+ #   <head>
+ #     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ #     <meta name="generator" content="Plack::App::Search" />
+ #     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ #     <title>
+ #       Search page
+ #     </title>
+ #     <style type="text/css">
+ # * {
+ # 	box-sizing: border-box;
+ # 	margin: 0;
+ # 	padding: 0;
+ # }
+ # .container {
+ # 	display: flex;
+ # 	align-items: center;
+ # 	justify-content: center;
+ # 	height: 100vh;
+ # }
+ # .search form {
+ # 	display: flex;
+ # 	align-items: center;
+ # }
+ # .search input[type="text"] {
+ # 	padding: 10px;
+ # 	border-radius: 4px;
+ # 	border: 1px solid #ccc;
+ # }
+ # .search button {
+ # 	margin-left: 10px;
+ # 	padding: 10px 20px;
+ # 	border-radius: 4px;
+ # 	background-color: #4CAF50;
+ # 	color: white;
+ # 	border: none;
+ # 	cursor: pointer;
+ # 	display: none;
+ # }
+ # .search button:hover {
+ # 	background-color: #45a049;
+ # }
+ # </style>
+ #   </head>
+ #   <body>
+ #     <div class="container">
+ #       <div class="search">
+ #         <form method="get" action="https://env.skim.cz">
+ #           <input type="text" autofocus="autofocus" />
+ #           <button type="submit" />
+ #         </form>
+ #       </div>
+ #     </div>
+ #   </body>
+ # </html>
 
 =head1 DEPENDENCIES
 
